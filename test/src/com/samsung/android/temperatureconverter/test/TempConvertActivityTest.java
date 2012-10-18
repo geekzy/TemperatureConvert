de7619c4f969b63ae569cd5536fae0142cf645d9
Imam Kurniawan <geekzy@gmail.com>
2012-10-18 14:24:46 +0700

    initial project import

---
+++ test/src/com/samsung/android/temperatureconverter/test/TempConvertActivityTest.java
@@ -0,0 +1,36 @@
+package com.samsung.android.temperatureconverter.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.samsung.android.temperatureconverter.R;
+import com.samsung.android.temperatureconverter.TempConvertActivity;
+
+public class TempConvertActivityTest extends
+ActivityInstrumentationTestCase2<TempConvertActivity> {
+
+	private TempConvertActivity mActivity;
+
+	public TempConvertActivityTest() {
+		super(TempConvertActivity.class);
+		setName("TempConvertActivityTest");
+	}
+
+	public TempConvertActivityTest(Class<TempConvertActivity> activityClass) {
+		super(activityClass);
+	}
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		mActivity = getActivity();
+		assertNotNull(mActivity);
+	}
+
+	public void testlayout() {
+		TextView hello = (TextView) mActivity.findViewById(R.id.hello);
+		assertEquals("Hello world!", hello.getText().toString());
+	}
+
+}
